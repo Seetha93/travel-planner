f5eb3cd7c66b83b1bcc14ebd6a4160d5060540cf
Seetha93 <rsindhu1@gmail.com>
2020-06-15 18:24:27 -0700

    Service to get countries, cities and airports data

--- database/connection.go
+++ database/connection.go
@@ -28,13 +28,49 @@ func Connect() (db *sql.DB, err error) {
 	return db, err
 }
 
-func Insert(db *sql.DB, name string, code string) {
+func Insert(db *sql.DB, tableName string, name string, code string) {
 	id := 0
 	sqlStatement := `
-			INSERT INTO country (name, code, created_at)
+			INSERT INTO ` + tableName + ` (name, code, created_at)
 			VALUES ($1, $2, $3)
 			RETURNING id`
 	err := db.QueryRow(sqlStatement, name, code, time.Now()).Scan(&id)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("New record ID is:", id)
+}
+
+func getId(db *sql.DB, sqlStatement string) (id int) {
+	row := db.QueryRow(sqlStatement)
+	switch err := row.Scan(&id); err {
+	case sql.ErrNoRows:
+		fmt.Println("No rows were returned!")
+		return -1
+	case nil:
+		fmt.Println(id)
+		return id
+	default:
+		panic(err)
+	}
+}
+
+func InsertConnectingData(db *sql.DB, tableName string,
+	parent string, child string,
+	parentTableName string, childTableName string,
+	parentCoulmnName string, childColumnName string) {
+	id := 0
+	sqlStatement := `select id from ` + childTableName + ` where code = '` + child + `';`
+	childId := getId(db, sqlStatement)
+
+	sqlStatement = `select id from ` + parentTableName + ` where code = '` + parent + `';`
+	parentId := getId(db, sqlStatement)
+	sqlStatement = `
+			INSERT INTO ` + tableName + ` (` + parentCoulmnName + `, ` + childColumnName + `, created_at)
+			VALUES ($1, $2, $3)
+			RETURNING id`
+	err := db.QueryRow(sqlStatement, parentId, childId, time.Now()).Scan(&id)
 
 	if err != nil {
 		panic(err)
--- dbScripts/main.go
+++ dbScripts/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -14,10 +15,20 @@ type CountryResponse struct {
 	Code string
 }
 
-func main() {
+type CityResponse struct {
+	Name         string
+	Code         string
+	Country_code string
+}
+
+type AirportResponse struct {
+	Name      string
+	Code      string
+	City_code string
+}
+
+func getData(url string) (body []byte, err error) {
 	config := Config.LoadBasiDataScrapperConfig()
-	fmt.Println("Starting the application...")
-	url := "https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/data/en-GB/countries.json"
 	req, _ := http.NewRequest("GET", url, nil)
 
 	req.Header.Add("x-rapidapi-host", config.TravelApiHost)
@@ -25,29 +36,101 @@ func main() {
 	req.Header.Add("x-access-token", config.AccessToken)
 
 	res, err := http.DefaultClient.Do(req)
-	if err == nil {
+	if err != nil {
 		fmt.Println("Error in the GET request")
 	}
 
 	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
+	body, _ = ioutil.ReadAll(res.Body)
+	return body, err
+}
 
-	fmt.Println(res)
-	fmt.Println(string(body))
-
-	var decoded []CountryResponse
-	if err := json.Unmarshal(body, &decoded); err != nil {
+func getCountriesData(db *sql.DB, url string) {
+	body, err := getData(url)
+	if err != nil {
 		panic(err)
 	}
 
-	db, err := DB.Connect()
+	var decoded []CountryResponse
+	if err := json.Unmarshal(body, &decoded); err != nil {
+		fmt.Println(body)
+		panic(err)
+	}
+
 	for _, record := range decoded {
 		fmt.Printf("Name: %s Code: %s\n", record.Name, record.Code)
-		DB.Insert(db, record.Name, record.Code)
+		DB.Insert(db, "country", record.Name, record.Code)
 
 		fmt.Println("")
 	}
 	fmt.Println(decoded[0])
+
+}
+
+func getCitiesData(db *sql.DB, url string) {
+	body, err := getData(url)
+	if err != nil {
+		panic(err)
+	}
+
+	// fmt.Println(res)
+	// fmt.Println(string(body))
+
+	var decoded []AirportResponse
+	if err := json.Unmarshal(body, &decoded); err != nil {
+		fmt.Println(body)
+		panic(err)
+	}
+
+	for _, record := range decoded {
+		fmt.Printf("Name: %s Code: %s CityCode: %s\n", record.Name, record.Code, record.City_code)
+		DB.Insert(db, "city", record.Name, record.Code)
+		DB.InsertConnectingData(db, "country_cities", record.City_code, record.Code, "country", "city", "country_id", "city_id")
+
+		fmt.Println("")
+	}
+	fmt.Println(decoded[0])
+
+}
+
+func getAirportData(db *sql.DB, url string) {
+	body, err := getData(url)
+	if err != nil {
+		panic(err)
+	}
+
+	// fmt.Println(res)
+	// fmt.Println(string(body))
+
+	var decoded []AirportResponse
+	if err := json.Unmarshal(body, &decoded); err != nil {
+		fmt.Println(body)
+		panic(err)
+	}
+
+	for _, record := range decoded {
+		fmt.Printf("Name: %s Code: %s CityCode: %s\n", record.Name, record.Code, record.City_code)
+		DB.Insert(db, "airport", record.Name, record.Code)
+		DB.InsertConnectingData(db, "city_airports", record.City_code, record.Code, "city", "airport", "city_id", "airport_id")
+
+		fmt.Println("")
+	}
+	fmt.Println(decoded[0])
+}
+
+func main() {
+	db, err := DB.Connect()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Starting the application...")
+	url := "https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/data/en-GB/countries.json"
+	getCountriesData(db, url)
+	url = "https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/data/en-GB/cities.json"
+	getCitiesData(db, url)
+	url = "https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/data/en-GB/airports.json"
+	getAirportData(db, url)
 
 	fmt.Println("Terminating the application...")
 	DB.CloseConnection(db)
